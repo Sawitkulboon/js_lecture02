aa656e0fbfd538bd08f4ac23b5dbff684b9c85f7
Sawitkulboon <deebzxc12369zx@gmail.com>
2025-09-28 12:30:46 +0700

    ทำ 01 02 03 พื้นฐาน js

---
+++ test01.js
@@ -0,0 +1,27 @@
+// singgle line commit
+/*
+    multi line commit
+    ♀♂
+*/
+//data typ ชนิดข้แฃอมูล
+console.log(`hello...`)
+console.log(`AAAAA`)
+console.log(`BBBBB`)
+console.log(`CCCCC`)
+
+//number
+console.log(`111`)  //integer (int)
+console.log(`15.426879`)
+console.log(``)
+
+//nul
+console.log(`nul`) //ค่า nul
+
+//object เขียรอยู่ใน {}และมีรูปแบบเป็น key : value
+console.log({
+    name: `sombat`,
+    age: 20,
+    address
+
+
+})
---
+++ test03.js
@@ -0,0 +1,43 @@
+//operator
+//arithmetic opertor + - * / %
+
+console.log(20 % 2) // หารเอาเศษ
+console.log(2 % 20)
+console.log(0 % 500)
+//Comparison เปรียบเทียบ (true/false) ==!= > < > = <= ===!==
+//==, != ไม่สนใจ data type
+console.log(111 == `111`) //เปรียบเทียบ ตัวเลข กับ ข้อความ
+
+//===, !== สนใจ data type
+console.log(111 === `111`)
+
+//เปรียบเทียบข้อความ
+console.log(`Bangna` >= `Bangpoo`)
+console.log(`Sombat` > `Somjai`)
+
+//Logical operator ตรรกะ ! (not), && (and), || (or)
+//ข้อมูลที่ใช้จะมีแค่ true/false และผลที่ได้จะมีแค่ true/false
+console.log(!true) //false
+console.log(!false) //
+
+console.log(object)
+console.log(object)
+console.log(object)
+console.log(object)
+
+console.log(object)
+console.log(object)
+console.log(object)
+console.log(object)
+
+//*** Ternary operator เงื่อนไข ? ทำเมื่อเป็นจิง: ทำเมื่อเป็นเท็จ*/
+
+let data1 = 100 % 30 == `0` ? `Wow...` : `Hui...`
+
+//increment ++ เพิ่มตัวมันเองทีละ 1 /decrement -- ลดตัวมันเองทีละ 1
+let data = 10
+console.log(data2) //10
+++data
+data++
+data2++
+console.log(data2) //13
