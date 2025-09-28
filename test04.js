59e857e1fad38f18ffd48819a9552ac9b2f0d783
Sawitkulboon <deebzxc12369zx@gmail.com>
2025-09-28 13:54:53 +0700

    ทำ 06 07

---
+++ test04.js
@@ -0,0 +1,12 @@
+//control Statement
+//selectioni-Condition พิสูจตรวจสอบก่อนที่จะทำงานใดๆ
+//if, if-else, if-else-if, switch
+let x = 20
+
+if (x > 50) {
+    console.log(`hello...`)
+}
+
+
+//loop ทำงานเดิมๆ ซ้ำ
+// for, while, do-while
---
+++ test05.js
@@ -0,0 +1,11 @@
+// if-else พิสูจครั้งเดียว จริงทำอย่าง เท็จทำอีกอย่าง
+
+let data = `Bankok`
+
+if (data == ``) {
+    // จริงทำชอง if
+    console.log(`Hello Bangkok`)
+} else {
+    // 
+    console.log(`Hello World`)
+}
